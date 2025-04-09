1909b008c145ced3fc847e5c027008ebe9be5b8f
Enver Bilal Birinci <enver.birinci@inavitas.com>
2025-04-09 22:45:18 +0300

    Photo Controller

--- app.js
+++ app.js
@@ -1,13 +1,40 @@
 const express = require('express');
-const path = require('path');
-
+const mongoose = require('mongoose');
+const fileUpload = require('express-fileupload');
+const methodOverride = require('method-override');
+const ejs = require('ejs');
 const app = express();
 
-app.use(express.static('public'));
+const photoController = require('./controllers/photoControllers')
+const pageControllers = require('./controllers/pageControllers')
 
-app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'temp/index.html'));
-});
+//Templates Engine
+app.set('view engine', 'ejs');
+
+//Connect DB
+mongoose.connect('mongodb://localhost/pcat-test-db');
+
+//Middlerwares
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(fileUpload());
+app.use(
+  methodOverride('_method', {
+    methods: ['POST', 'GET'],
+  })
+);
+
+//ROUTES
+app.get('/', photoController.getAllPhotos);
+app.get('/photos/:id', photoController.getPhoto);
+app.post('/photos', photoController.createPhoto);
+app.put('/photos/:id', photoController.updatePhoto);
+app.delete('/photos/:id', photoController.deletePhoto);
+
+app.get('/about', pageControllers.getAboutPage);
+app.get('/add', pageControllers.getAddPage);
+app.get('/photos/edit/:id', pageControllers.getEditPage);
 
 const port = 3000;
 app.listen(port, () => {
